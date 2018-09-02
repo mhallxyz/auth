0216cfc48f4e22cc26fc6e16296ed720271bc95b
Mathew Hall <mhall.js@gmail.com>
2018-09-02 22:15:01 +0100

    Password field added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-
-import { Card, CardSection, Button } from './common';
+import firebase from 'firebase';
+import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  onButtonPress = () => {
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
-
+          <Input
+            placeholder='Enter your email'
+            label='Email'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
         </CardSection>
-        <CardSection>
 
-        </CardSection>
         <CardSection>
-          <Button>Login</Button>
+        <Input
+            secureTextEntry
+            placeholder='Enter your password'
+            label='Password'
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onButtonPress}>Login</Button>
         </CardSection>
       </Card>
     )
