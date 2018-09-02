12c0e1276f27745b1280fe3ba3140024552a3252
Mathew Hall <mhall.js@gmail.com>
2018-09-02 19:19:46 +0100

    Firebase rolled back to 5.0.3 due to incompatibility with React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAkHd7gvQ_2F6Qmqdu7CQ2KFIlVphBmskc",
+      authDomain: "auth-993e4.firebaseapp.com",
+      databaseURL: "https://auth-993e4.firebaseio.com",
+      projectId: "auth-993e4",
+      storageBucket: "auth-993e4.appspot.com",
+      messagingSenderId: "605489078241"
+    });
+  }
+
   render() {
     return(
       <View>
         <Header headerText='Auth' />
-        <Text>An app</Text>
+        <LoginForm />
       </View>
     )
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+import { Card, CardSection, Button } from './common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+
+        </CardSection>
+        <CardSection>
+
+        </CardSection>
+        <CardSection>
+          <Button>Login</Button>
+        </CardSection>
+      </Card>
+    )
+  }
+}
+
+export default LoginForm;
